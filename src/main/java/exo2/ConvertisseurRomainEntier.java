9920574e05b0459c89dba2bc1399451a94fca3e5
ZihaoZHENG <zihaoz96@gmail.com>
2019-11-13 17:22:19 +0100

    shouldReturn5WhenV réussi

--- src/main/java/exo2/ConvertisseurRomainEntier.java
+++ src/main/java/exo2/ConvertisseurRomainEntier.java
@@ -4,6 +4,8 @@ public class ConvertisseurRomainEntier {
 	public int getResultat(String valeur) {
 		if (valeur == "I") {
 			return 1;
+		}else if (valeur == "V") {
+			return 5;
 		}
 		
 		return 0;	
