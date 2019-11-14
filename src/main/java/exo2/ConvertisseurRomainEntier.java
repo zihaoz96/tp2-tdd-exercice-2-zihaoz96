0b3f528022b894248ae180c3807f148387f70cab
ZihaoZHENG <zihaoz96@gmail.com>
2019-11-14 12:32:14 +0100

    Factorisation par switch

--- src/main/java/exo2/ConvertisseurRomainEntier.java
+++ src/main/java/exo2/ConvertisseurRomainEntier.java
@@ -1,19 +1,30 @@
 package exo2;
 
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
 public class ConvertisseurRomainEntier {
+	
 	public int getResultat(String valeur) {
-		if (valeur == "I") {
-			return 1;
-		}else if (valeur == "V") {
-			return 5;
-		}else if (valeur == "X") {
-			return 10;
-		}else if (valeur == "L") {
-			return 50;
-		}else if (valeur == "C") {
-			return 100;
-		}
 		
-		return 0;	
+		switch (valeur) {
+		case "I":
+			return 1;
+			
+		case "V":
+			return 5;
+			
+		case "X":
+			return 10;
+			
+		case "L":
+			return 50;
+			
+		case "C":
+			return 100;
+			
+		default:
+			return 0;
+		
+		}
 	}
 }
